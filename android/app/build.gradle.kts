6c0c7373d36165d011cb3843d52286b60c8d542c
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-03 14:42:34 +0000

    fix: app namcespace changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.weather_app_openweathermap"
+    namespace = "com.ephitech.weather_lite"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.weather_lite.weather_app_openweathermap"
+        applicationId = "com.ephitech.weather_lite"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
