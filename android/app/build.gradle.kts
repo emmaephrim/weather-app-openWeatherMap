fe8c54639a81db97bc8b21bdbefbe6e50065f059
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-03 10:46:35 +0000

    feat: launcher icons generated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.weather_app_openweathermap"
+        applicationId = "com.weather_lite.weather_app_openweathermap"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -41,4 +41,11 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+    // ...
+    implementation("com.google.android.material:material:1.14.0-alpha09")
+    // ...
 }
